088aa13924ac50987889dc516c00994d056dea11
Thomas <thomasprof21@gmail.com>
2023-10-02 20:50:14 -0300

    Primeiro commit

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String exception) {
+        System.out.println("O segundo parametro tem que ser maior que o primeiro");
+    }
+}
